99b62d1293fb0bfb7736a3331f0cb487a2617ab2
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-17 11:15:49 +0700

    error

---
+++ BankTransferConfig.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Modul8_103022300160
+{
+    internal class BankTransferConfig
+    {
+        class Transfer
+        {
+            public int treshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+        }
+        class Confirmation
+        {
+            public int en { get; set; }
+            public int id { get; set; }
+        }
+        class Config
+        {
+            public string lang { get; set; }
+            public Transfer transfer { get; set; }
+            public string[] methods { get; set; }
+            public Confirmation confirmation { get; set; }
+
+            public Config(){}
+            public void ReadConfigFile()
+            {
+                string configJsonData = File.ReadAllText(filePath);
+                Config config = JsonSerializer.Deserialize<Config>(configJsonData);
+
+                lang = config.lang; 
+                transfer = config.transfer;
+                methods = config.methods;
+                confirmation = config.confirmation;
+                
+            }
+
+        }
+
+            public const String filePath = "D:\\Modul8_103022300160\\bank_transfer_config.json";
+            public config_bank()
+            {
+                try
+                {
+                    ReadConfigFile();
+                }
+                catch (Exception)
+                {
+                    setDefault();
+                    WriteNewConfigFile();
+                }
+            }
+            
+
+            public void setDefault()
+            {
+                ReadConfigFile();
+            }
+            public void WriteNewConfigFile()
+            {
+                JsonSerializerOptions options = new JsonSerializerOptions()
+                {
+                    WriteIndented = true
+                };
+                String jsonString = JsonSerializer.Serialize(bankTransferConfig, options);
+                File.WriteAllText(filePath, jsonString);
+            }
+        
+
+        public void ubahBahasa(string lang, BankTransferConfig data)
+        {
+            lang = (lang == null) ? "en" : "id";
+            ReadConfigFile();
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("modul 8");
+using System.Security.Cryptography.X509Certificates;
+using Modul8_103022300160;
+class Program
+{
+    static void Main()
+    {
+        
+        BankTransferConfig config = new BankTransferConfig();
+        
+        if(config.lang = "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer: ");
+        }
+        else
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer: ");
+        }
+        
+    }
+}
